7d7b30052c22701b8882e690d9c11fe864b78d7d
soumilk <soumilk.k@gmail.com>
2020-02-05 00:18:31 +0530

    Updates

---
+++ precache-manifest.466f22eb77be6d5e0512ffe0d19cc72f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b0f4c85545a6d9ff2c6779d42a1678",
+    "url": "/React-demo-Monsters/index.html"
+  },
+  {
+    "revision": "553fd4dc95ef30a47efa",
+    "url": "/React-demo-Monsters/static/css/main.720c19d7.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/React-demo-Monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-demo-Monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553fd4dc95ef30a47efa",
+    "url": "/React-demo-Monsters/static/js/main.6299212c.chunk.js"
+  },
+  {
+    "revision": "8361805215377ae22c02",
+    "url": "/React-demo-Monsters/static/js/runtime-main.d1e39084.js"
+  }
+]);
